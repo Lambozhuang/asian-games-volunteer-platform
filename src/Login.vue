e09ec0c51b093a1a9248a2e368ce17ddf7664e6f
Lambozhuang <lambozhuang@icloud.com>
2022-05-30 20:24:14 +0800

    登录页面样式微调

--- src/Login.vue
+++ src/Login.vue
@@ -11,38 +11,50 @@
         margin-top: 100px;
       "
     >
-      <n-space justify="space-between">
-        <n-space vertical align="start">
-          <div class="login-title">志愿者管理平台</div>
-          <div class="login-description1">2022杭州亚运会</div>
-          <div class="login-description2">管理员登录入口</div>
-        </n-space>
-        <n-space vertical align="center">
-          <n-input
-            class="login-input"
-            type="text"
-            placeholder="用户"
-            v-model:value="userName"
-          ></n-input>
-          <n-input
-            class="login-input"
-            type="password"
-            show-password-on="mousedown"
-            placeholder="密码"
-            v-model:value="password"
-          ></n-input>
-          <n-button class="login-button" type="primary" @click="login">
-            登录
-          </n-button>
-        </n-space>
-      </n-space>
+      <n-grid cols="10" :x-gap="12">
+        <n-grid-item span="5">
+          <n-space vertical align="start">
+            <div class="login-title">志愿者管理平台</div>
+            <div class="login-description1">2022杭州亚运会</div>
+            <div class="login-description2">管理员登录入口</div>
+          </n-space>
+        </n-grid-item>
+        <n-grid-item span="5">
+          <n-space vertical align="center" justify="center">
+            <n-input
+              class="login-input"
+              type="text"
+              placeholder="用户"
+              v-model:value="userName"
+            ></n-input>
+            <n-input
+              class="login-input"
+              type="password"
+              show-password-on="mousedown"
+              placeholder="密码"
+              v-model:value="password"
+            ></n-input>
+            <n-button class="login-button" type="primary" @click="login">
+              登录
+            </n-button>
+          </n-space>
+        </n-grid-item>
+      </n-grid>
     </n-card>
   </n-layout>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { NButton, NInput, NSpace, NCard, NLayout } from "naive-ui";
+import {
+  NButton,
+  NInput,
+  NSpace,
+  NCard,
+  NLayout,
+  NGrid,
+  NGridItem,
+} from "naive-ui";
 import { useRouter } from "vue-router";
 import axios from "axios";
 import common from "./Common.vue";
@@ -82,6 +94,7 @@ function login() {
 .login-button,
 .login-input {
   min-width: 200px;
+  max-width: 200px;
 }
 
 .login-title {
